a97547c988aa0d58b5cab98830145a706ee21668
Darlan Brandt <darlanbrandt@users.noreply.github.com>
2018-06-19 18:26:51 -0300

    Inserir jQuery para máscara de preenchimento

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+	$(document).ready(function(){
+		$('.cpf').mask('000.000.000-00');
+	});
+})(jQuery);
+
+(function ($) {
+            $(document).ready(() => {
+              $('.message').delay(5000).fadeOut(1000);
+              });
+        })(jQuery);
