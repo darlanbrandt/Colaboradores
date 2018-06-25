752c521e9a5ae0e23973693a6fedad11b2b46474
Djan Belli <djanbell@amazon.co.uk>
2018-06-25 22:39:20 +0100

    wip

--- routes/json.js
+++ routes/json.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var colaborador = require("../controllers/ColaboradorController.js");
 
 // Lista todos os colaboradores
-router.get('/', function(req, res) {
+router.get('/list', function(req, res) {
   colaborador.listJson(req, res);
 });
 
