50955b22237bee3f9198e1429c661d387d456bb0
Darlan Brandt <darlanbrandt@users.noreply.github.com>
2018-06-28 02:21:45 -0300

    Ajuste jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,17 @@
 (function ($) {
 	$(document).ready(function(){
-    $('#cpf').mask('000.000.000-00');
-    $("#message").delay(3500).hide();
-	});
+		$('#cpf').mask('000.000.000-00');
+  });
+  
+  $(document).ready(function(){
+    $('.telefone').mask('(00)00000-0000');
+  });
 })(jQuery);
+
+
+
+(function ($) {
+            $(document).ready(() => {
+              $('.message').delay(5000).fadeOut(1000);
+              });
+        })(jQuery);
